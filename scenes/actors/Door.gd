a970ded7cd79aff2765e523cb09029f552ece267
Henrique L. Alves <henriquelalves@gmail.com>
2016-12-08 19:16:10 -0200

    Corrected door behavior.

--- scenes/actors/Door.gd
+++ scenes/actors/Door.gd
@@ -8,8 +8,8 @@ func _ready():
 
 func _fixed_process(delta):
 	if(get_overlapping_areas().size() > 0) and opening == false:
-		opening = true
 		if get_node("/root/global").player_keys > 0:
+			opening = true
 			get_node("/root/global").player_keys -= 1
 			get_node("AnimationPlayer").play("open")
 
